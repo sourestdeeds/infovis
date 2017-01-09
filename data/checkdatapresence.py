6db42809e04e9d03ea94331886fe5d88d03570b0
Jef Timmermans <jef.timmermans.147@gmail.com>
2017-01-09 13:01:27 +0100

    Selected useful planet parameters for method quality visualisation

--- data/checkdatapresence.py
+++ data/checkdatapresence.py
@@ -5,20 +5,17 @@ planet property is present for planets discovered by a certain method.
 
 
 from csv import DictReader
+import json
 
 
 def parse():
-    columns = None
+    columns = ['pl_bmasse', 'pl_dens', 'pl_eqt', 'pl_imppar', 'pl_impparlim', 'pl_insol', 'pl_occdep', 'pl_orbeccen', 'pl_orbincl', 'pl_orblper', 'pl_orbper', 'pl_orbsmax', 'pl_orbtper', 'pl_rade', 'pl_ratdor', 'pl_ratror', 'pl_rvamp']
     data = {}
 
     with open('planets.csv', 'r') as f:
         reader = DictReader(filter(lambda line: not line.startswith('#'), f))
         
         for row in reader:
-            if not columns:
-                columns = [c for c in row.keys() if c != 'pl_discmethod' and c.startswith('pl_')]
-                columns.sort()
-        
             method = row['pl_discmethod']
             
             if method not in data:
@@ -54,7 +51,17 @@ def main():
                 fractions.append(str(fraction) + '%')
             
             print(formatstring % ((methods[method] + ' (%4d)' % total), *fractions), file = f)
-
+        
+        print('', file = f)
+        avg = {method: (sum(data[method][1].values()) / (data[method][0] * len(columns))) for method in data}
+        lower, upper = min(avg.values()), max(avg.values())
+        
+        for entry in avg:
+            avg[entry] -= lower
+            avg[entry] /= (upper - lower)
+        
+        print(json.dumps(avg, indent = 4), file = f)
+        
 
 if __name__ == '__main__':
     main()
